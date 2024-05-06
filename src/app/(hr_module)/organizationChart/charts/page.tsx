c0fd5b81a9a42f4ed4bd922c346f2cbbca599320
mohit-kandoi <mohitkandoi65@gmail.com>
2024-05-06 10:33:12 +0530

    handling dcoument error

--- src/app/(hr_module)/organizationChart/charts/page.tsx
+++ src/app/(hr_module)/organizationChart/charts/page.tsx
@@ -1,43 +1,8 @@
-'use client';
-import React from 'react';
-import { Tree, TreeNode } from 'react-organizational-chart';
-import { users } from './data.js';
-import Avatar from './avatar';
+import dynamic from 'next/dynamic';
 
-const data = {
-  name: 'CEO',
-  children: [
-    {
-      name: 'Manager 1',
-      children: [{ name: 'Employee 1' }, { name: 'Employee 2' }],
-    },
-    {
-      name: 'Manager 2',
-      children: [{ name: 'Employee 3' }, { name: 'Employee 4' }],
-    },
-  ],
-};
+const MyComponentNoSSR = dynamic(() => import('./tree'), {
+  ssr: false,
+});
 
-const ExampleTree = () => (
-  <>
-    <Tree label={<Avatar src={users[0].avatar} />}>
-      <TreeNode label={<Avatar src={users[0].avatar} />}>
-        <TreeNode label={<Avatar src={users[0].avatar} />}>
-          <TreeNode label={<Avatar src={users[0].avatar} />}>
-            <TreeNode label={<Avatar src={users[0].avatar} />}>
-              <TreeNode label={<Avatar src={users[0].avatar} />}>
-                <TreeNode label={<Avatar src={users[0].avatar} />} />
-              </TreeNode>
-            </TreeNode>
-          </TreeNode>
-        </TreeNode>
-        <TreeNode label={<Avatar src={users[0].avatar} />} />
-      </TreeNode>
-      <TreeNode label={<Avatar src={users[0].avatar} />}>
-        <TreeNode label={<Avatar src={users[0].avatar} />} />
-      </TreeNode>
-    </Tree>
-    {/* <div>Organization Chart</div> */}
-  </>
-);
-export default ExampleTree;
+const Page = () => <MyComponentNoSSR />;
+export default Page;
